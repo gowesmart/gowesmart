74a05ecee6e950344e8bcba4846ae4c017fedcce
ikhsan <ikhsanapriliano4@gmail.com>
2024-08-07 12:53:50 +0700

    feat(TASK002):add loading page server side

---
+++ app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className="flex flex-col h-screen justify-center items-center">
+            <i className="fa-solid fa-bicycle fa-bounce text-[64px]"></i>
+            <p className="font-bold text-[20px]">gowesmart</p>
+        </div>
+    )
+}
+
+export default Loading
