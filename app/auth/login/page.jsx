5cc37692172b6a298bedc597a2e9afb4396bce36
raihanmd <100277303+raihanmd@users.noreply.github.com>
2024-08-09 17:30:51 +0700

    fix(TASK004): reset password service (#21)

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,22 +1,15 @@
-import FormLogin from '@/components/auth/FormLogin';
-import Link from 'next/link';
+import FormLogin from "@/components/auth/FormLogin";
 
 export default function Login() {
   return (
-    <main className="pt-[80px] min-h-dvh flex justify-center items-center">
+    <main className="flex min-h-dvh items-center justify-center pt-[80px]">
       <div>
-        <div className="flex flex-col gap-8 p-12 w-[30rem] rounded-lg border">
+        <div className="flex w-[30rem] flex-col gap-8 rounded-lg border border-accent bg-primary p-12">
           <div className="w-full max-w-lg">
             <h1 className="text-2xl font-bold">Login</h1>
             <p>enter your email and password to use our platform</p>
           </div>
           <FormLogin />
-          <p>
-            Don't have an account?{' '}
-            <Link href={'/auth/register'} className="text-secondary">
-              Register Now
-            </Link>
-          </p>
         </div>
       </div>
     </main>
