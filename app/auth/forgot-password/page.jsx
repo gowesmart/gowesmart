8b80f58b033fcd0a7d95aa78f00d88f923302aa4
ikhsan <ikhsanapriliano4@gmail.com>
2024-08-11 19:45:10 +0700

    feat(TASK002): add responsive design

--- app/auth/forgot-password/page.jsx
+++ app/auth/forgot-password/page.jsx
@@ -4,7 +4,7 @@ export default function ForgotPassword() {
   return (
     <main className="flex min-h-dvh items-center justify-center pt-[80px]">
       <div>
-        <div className="flex w-[30rem] flex-col gap-8 rounded-lg border border-accent bg-primary p-12">
+        <div className="flex w-full md:w-[30rem] flex-col gap-8 rounded-lg md:border border-accent bg-primary p-12">
           <div className="w-full max-w-lg">
             <h1 className="text-2xl font-bold">Forgot Password</h1>
             <p>enter your username and email for requesting change password</p>
