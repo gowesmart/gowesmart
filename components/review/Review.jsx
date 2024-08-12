ed7e2042f32001250e69151f859e0ac07265f9c2
raihanmd <100277303+raihanmd@users.noreply.github.com>
2024-08-12 13:00:02 +0700

    feat: add history trx page for logged in user(#34)

--- components/review/Review.jsx
+++ components/review/Review.jsx
@@ -1,4 +1,21 @@
-const Review = () => {
+const Review = ({ review }) => {
+  const renderStars = (rating) => {
+    const totalStars = 5;
+    let stars = [];
+
+    for (let i = 1; i <= totalStars; i++) {
+      stars.push(
+        <i
+          key={i}
+          aria-hidden
+          className={`fa-solid fa-star ${i <= rating ? "text-yellow-400" : "text-gray-400"}`}
+        ></i>,
+      );
+    }
+
+    return stars;
+  };
+
   return (
     <div className="flex w-full flex-col justify-center gap-5 rounded-md border border-accent p-11">
       <div className="flex items-center gap-5">
@@ -6,20 +23,16 @@ const Review = () => {
           <i aria-hidden className="fa-solid fa-user"></i>
         </div>
         <div className="flex h-[50px] flex-col justify-center">
-          <p className="text-[20px]">username</p>
+          <p className="text-[20px]">{review?.user_username}</p>
           <div className="flex items-center gap-2 text-[12px]">
-            <div className="flex gap-1">
-              <i aria-hidden className="fa-solid fa-star text-yellow-400"></i>
-              <i aria-hidden className="fa-solid fa-star text-yellow-400"></i>
-              <i aria-hidden className="fa-solid fa-star text-yellow-400"></i>
-              <i aria-hidden className="fa-solid fa-star text-yellow-400"></i>
-              <i aria-hidden className="fa-solid fa-star text-gray"></i>
-            </div>
-            <p>(4)</p>
+            <div className="flex gap-1">{renderStars(review?.rating || 0)}</div>
+            <p>({review?.rating || 0})</p>
           </div>
         </div>
       </div>
-      <div className="h-[220px] border border-accent bg-primary"></div>
+      <div className="mih-h-[220px] border border-accent bg-primary p-3">
+        <p>{review?.comment}</p>
+      </div>
     </div>
   );
 };
